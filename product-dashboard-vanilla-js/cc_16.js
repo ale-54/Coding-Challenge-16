5b949f8251f8fe48dc47d23a5d3418b4209ccfda
Ashley Le <ashleyle@usf.edu>
2025-03-30 19:47:51 -0400

    Implement fetchProductsThen using .then() and .catch()

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -1,1 +1,20 @@
 //Task 1: Set Up the Project 
+
+//Task 2
+function fetchProductsThen() {
+    fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error has occurred: ${response.status}`); //getting product data 
+        }
+        return response.json();
+    })
+    .then(products => {
+        products.forEach(product => {
+            console.log(product.fields.name); //logging each product's name in the console
+        });
+    })
+    .catch(error => {
+        console.error('Error has occurred:', error); //logs the actual errors
+    });
+};
